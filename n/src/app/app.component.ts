5bafd8976428ff3a599128d8a7914b71ffdcfbee
mikapeu <mikaelsposito2@outlook.com>
2022-05-27 21:15:06 -0300

    ultima vez

--- n/src/app/app.component.ts
+++ n/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {  
+export class AppComponent {   
   title = 'todo';  
   
   filter: 'all' | 'active' | 'done' = 'all';  
